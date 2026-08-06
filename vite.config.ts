383425c33947a145b95b1cc317db91c364093fe9
limengjiao <1159689378@qq.com>
2026-08-06 09:03:17 -0500

    Support Vercel and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/marking-menu/' : '/',
+export default defineConfig(() => ({
+  base: process.env.GITHUB_ACTIONS ? '/marking-menu/' : '/',
   plugins: [react()],
 }))
